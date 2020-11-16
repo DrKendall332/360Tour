e3601c97016af2920aacadabb6f00184a01b3a92
DrKendall332 <67517792+DrKendall332@users.noreply.github.com>
2020-11-16 12:29:13 +0000

    Add files via upload

--- Images/s3_32.js
+++ Images/s3_32.js
@@ -3,35 +3,47 @@ var data = {
 	height: 2688,
   };
   
-var markers = [{
-    id: 'Doorway',
-    y: 1414,
-    x: 4782,
-    image: 'assets/forward_arrow.png',
-    width: 32,
-    height: 32,
-    tooltip: 'To Science Corridor',
-    scale: [1,2],
-	data: {type: '3Droom', room: 'science1', id:'S3.32', x:0, y:0}
-  },
-  {
-    id: 'HazardsActivity',
-    y: 1342,
-    x: 5360,
-    image: 'assets/think.png',
-    width: 32,
-    height: 32,
-    tooltip: 'Hazards Activity',
-    scale: [1,2],
-    content: '<iframe style="width:95%; height:95%" id="gameFrame" src="../Games/matchinggame.html"></iframe>',
-    data: {type: 'gameFrame'}
-  },
-  ];
+var markers = {
+	default:[{
+	    id: 'Doorway',
+	    y: 1414,
+	    x: 4782,
+	    image: 'assets/forward_arrow.png',
+	    width: 32,
+	    height: 32,
+	    tooltip: 'To Science Corridor',
+	    scale: [1,2],
+	    data: {type: '3Droom', room: 'science1', id:'S3.32', x:0, y:0}
+  	},],
+	transition:[{
+	    id: 'Doorway',
+	    y: 1414,
+	    x: 4782,
+	    image: 'assets/forward_arrow.png',
+	    width: 32,
+	    height: 32,
+	    tooltip: 'To Science Corridor',
+	    scale: [1,2],
+	    data: {type: '3Droom', room: 'science1', id:'S3.32', x:0, y:0}
+  	},
+	{
+	    id: 'HazardsActivity',
+	    y: 1342,
+	    x: 5360,
+	    image: 'assets/think.png',
+	    width: 32,
+	    height: 32,
+	    tooltip: 'Hazards Activity',
+	    scale: [1,2],
+	    content: '<iframe style="width:95%; height:95%" id="gameFrame" src="../Games/matchinggame.html"></iframe>',
+	    data: {type: 'gameFrame'}
+	  },],
+   }
 
 function getData() {
   return {markers:markers, data:data};
 }
 
-function runMarkerUpdate() {
-  markersPlugin.setMarkers(markers);
+function runMarkerUpdate(tourID) {
+  markersPlugin.setMarkers(markers[tourID]);
 }
