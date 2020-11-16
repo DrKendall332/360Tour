cdefee328ddeab2cbc22b0e75b684844ec360086
DrKendall332 <67517792+DrKendall332@users.noreply.github.com>
2020-11-16 12:37:54 +0000

    Add files via upload

--- Images/s3_32.js
+++ Images/s3_32.js
@@ -16,17 +16,6 @@ var markers = {
 	    data: {type: '3Droom', room: 'science1', id:'S3.32', x:0, y:0}
   	},],
 	transition:[{
-	    id: 'Doorway',
-	    y: 1414,
-	    x: 4782,
-	    image: 'assets/forward_arrow.png',
-	    width: 32,
-	    height: 32,
-	    tooltip: 'To Science Corridor',
-	    scale: [1,2],
-	    data: {type: '3Droom', room: 'science1', id:'S3.32', x:0, y:0}
-  	},
-	{
 	    id: 'HazardsActivity',
 	    y: 1342,
 	    x: 5360,
@@ -40,10 +29,17 @@ var markers = {
 	  },],
    }
 
+var exclude = {};
+
 function getData(tourID) {
   return {markers:markers[tourID], data:data};
 }
 
 function runMarkerUpdate(tourID) {
-  markersPlugin.setMarkers(markers[tourID]);
+	if {exclude[tourID]) {
+	  markersPlugin.setMarkers(markers[tourID]);
+	}
+	else {
+	  markersPlugin.setMarkers(markers.default.concat(markers[tourID]));
+	}
 }
