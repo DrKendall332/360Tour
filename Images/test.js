25925ab656b6d544046331ecc520ddc4dd344ecc
DrKendall332 <67517792+DrKendall332@users.noreply.github.com>
2020-11-23 12:52:58 +0000

    Create test.js

---
+++ Images/test.js
@@ -0,0 +1,5 @@
+var myDoors = [{width:550, height:100, left:160, top:110, day:114, date:24, youtube:'38XWz1IL6j8'},
+{width:550, height:100, left:1160, top:110, day:1, date:25, youtube:'38XWz1IL6j8'},
+{width:550, height:100, left:160, top:410, day:3, date:26, youtube:'38XWz1IL6j8'},
+{width:550, height:100, left:560, top:810, day:5, date:27, youtube:'38XWz1IL6j8'},
+{width:550, height:100, left:1160, top:610, day:2, date:28, youtube:'38XWz1IL6j8'}];
