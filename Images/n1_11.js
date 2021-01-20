ad7e1cfa8e49b1cccae8d2095adf13070fa9e6bf
DrKendall332 <67517792+DrKendall332@users.noreply.github.com>
2021-01-20 16:25:52 +0000

    Update n1_11.js

--- Images/n1_11.js
+++ Images/n1_11.js
@@ -14,6 +14,17 @@ var markers = [{
     scale: [1,2],
 	data: {type: '3Droom', room: 'art2', id:'N1.11', x:0, y:0}
   },
+  {
+    id: 'ArtsVid',
+    y: 1390,
+    x: 2625,
+    image: 'assets/speech.png',
+    width: 32,
+    height: 32,
+    tooltip: 'Art Introduction',
+    scale: [1,2],
+    data: {type: 'gameFrame', html: '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/QFODbKQzQFU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'}
+  },
   ];
 
 function getData() {
