6abfd2a7878e6a9c8a25d7730cf8b1a2657fc473
DrKendall332 <67517792+DrKendall332@users.noreply.github.com>
2021-01-20 15:33:52 +0000

    Update n3_47.js

--- Images/n3_47.js
+++ Images/n3_47.js
@@ -14,6 +14,18 @@ var markers = [{
     scale: [1,2],
 	data: {type: '3Droom', room: 'maths1', id:'N3.47', x:0, y:0}
   },
+  {
+    id: 'Doorway',
+    y: 1434,
+    x: 2501,
+    image: 'assets/forward_arrow.png',
+    width: 32,
+    height: 32,
+    tooltip: 'Mrs McGillivray',
+    scale: [1,2],
+    data: {type: 'gameFrame', html: '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/EY02qFRrV-k" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'}
+  },
+	       
   ];
 
 function getData() {
