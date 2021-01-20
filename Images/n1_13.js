0f8963a42d02aa8f3102025cbd526907d75949db
DrKendall332 <67517792+DrKendall332@users.noreply.github.com>
2021-01-20 16:29:22 +0000

    Update n1_13.js

--- Images/n1_13.js
+++ Images/n1_13.js
@@ -14,6 +14,17 @@ var markers = [{
     scale: [1,2],
 	data: {type: '3Droom', room: 'he', id:'N1.13', x:0, y:0}
   },
+  {
+    id: 'HEVid',
+    y: 1411,
+    x: 5003,
+    image: 'assets/speech.png',
+    width: 32,
+    height: 32,
+    tooltip: 'Home Economics Introduction',
+    scale: [1,2],
+    data: {type: 'gameFrame', html: '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/I6a01cz3q_4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'}
+  },
   ];
 
 function getData() {
