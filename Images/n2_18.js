c6f4e951675bfe07d43187c2a1ea389d0142b251
DrKendall332 <67517792+DrKendall332@users.noreply.github.com>
2021-01-20 16:08:11 +0000

    Update n2_18.js

--- Images/n2_18.js
+++ Images/n2_18.js
@@ -14,6 +14,28 @@ var markers = [{
     scale: [1,2],
 	data: {type: '3Droom', room: 'engcorridor', id:'N2.18', x:0, y:0}
   },
+  {
+    id: 'EngVid',
+    y: 1524,
+    x: 4865,
+    image: 'assets/speech.png',
+    width: 32,
+    height: 32,
+    tooltip: 'English Introduction',
+    scale: [1,2],
+    data: {type: 'gameFrame', html: '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/GHaAkzdsv60" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'}
+  },
+  {
+    id: 'BookVid',
+    y: 1813,
+    x: 83,
+    image: 'assets/speech.png',
+    width: 32,
+    height: 32,
+    tooltip: 'Reading Introduction',
+    scale: [1,2],
+    data: {type: 'gameFrame', html: '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/2Oxh4uG2bRs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'}
+  },
   ];
 
 function getData() {
