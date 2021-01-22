a8710fb09461c1cbf0bb0e17a0fe8dd4a030074b
DrKendall332 <67517792+DrKendall332@users.noreply.github.com>
2021-01-22 15:38:46 +0000

    Update foyer.js

--- Images/foyer.js
+++ Images/foyer.js
@@ -26,6 +26,106 @@ var markers = [
     scale: [1,2],
 	data: {type: 'url', url:'index.html'}
   },
+  {
+    id: 'GuidanceJump',
+    y: 1443,
+    x: 2380,
+    image: 'assets/forward_arrow.png',
+    width: 32,
+    height: 32,
+    tooltip: 'Jump to Guidance!',
+    scale: [1,2],
+	data: {type: '3Droom', room: 'guidance', x:2471, y:1250}
+  },
+  {
+    id: 'ScienceJump',
+    y: 1370,
+    x: 1900,
+    image: 'assets/forward_arrow.png',
+    width: 32,
+    height: 32,
+    tooltip: 'Jump to Science!',
+    scale: [1,2],
+	data: {type: '3Droom', room: 's3_32', x:0, y:1355}
+  },
+  {
+    id: 'RMPSJump',
+    y: 1370,
+    x: 2300,
+    image: 'assets/forward_arrow.png',
+    width: 32,
+    height: 32,
+    tooltip: 'Jump to RMPS!',
+    scale: [1,2],
+	data: {type: '3Droom', room: 'n3_47', x:4833, y:1496}
+  },
+
+  {
+    id: 'ASNJump',
+    y: 1370,
+    x: 2220,
+    image: 'assets/forward_arrow.png',
+    width: 32,
+    height: 32,
+    tooltip: 'Jump to Support for Learning!',
+    scale: [1,2],
+	data: {type: '3Droom', room: 'n3landing', x:120, y:1311}
+  },
+  {
+    id: 'MathsJump',
+    y: 1370,
+    x: 2380,
+    image: 'assets/forward_arrow.png',
+    width: 32,
+    height: 32,
+    tooltip: 'Jump to Maths!',
+    scale: [1,2],
+	data: {type: '3Droom', room: 'maths1', x:2043, y:1381}
+  },
+  {
+    id: 'EnglishJump',
+    y: 1370,
+    x: 2460,
+    image: 'assets/forward_arrow.png',
+    width: 32,
+    height: 32,
+    tooltip: 'Jump to English!',
+    scale: [1,2],
+	data: {type: '3Droom', room: 'n2_18', x:4865, y:1524}
+  },
+  {
+    id: 'HEJump',
+    y: 1370,
+    x: 2860,
+    image: 'assets/forward_arrow.png',
+    width: 32,
+    height: 32,
+    tooltip: 'Jump to Home Economics!',
+    scale: [1,2],
+	data: {type: '3Droom', room: 'n1_13', x:5003, y:1411}
+  },
+  {
+    id: 'ArtJump',
+    y: 1370,
+    x: 2620,
+    image: 'assets/forward_arrow.png',
+    width: 32,
+    height: 32,
+    tooltip: 'Jump to Art!',
+    scale: [1,2],
+	data: {type: '3Droom', room: 'n1_11', x:2625, y:1390}
+  },
+  {
+    id: 'PEJump',
+    y: 1370,
+    x: 2780,
+    image: 'assets/forward_arrow.png',
+    width: 32,
+    height: 32,
+    tooltip: 'Jump to Physical Education!',
+    scale: [1,2],
+	data: {type: '3Droom', room: 'pe2', x:1363, y:1279}
+  },
   ];
 
 function getData() {
