3b5711ce0e0b21394c3aa007dfba7544c160c82c
DrKendall332 <67517792+DrKendall332@users.noreply.github.com>
2021-01-26 09:24:29 +0000

    Update soccorridor.js

--- Images/soccorridor.js
+++ Images/soccorridor.js
@@ -58,7 +58,7 @@ var markers = [{
     scale: [1,2],
 	data: {type: '3Droom', room: 'n2_24', id:'Doorway', x:0, y:0}
   },
-  {
+  /*{
     id: 'SocialSubjVid',
     y: 1273,
     x: 8,
@@ -68,7 +68,7 @@ var markers = [{
     tooltip: 'Social Subjects Introduction',
     scale: [1,2],
     data: {type: 'gameFrame', html: '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/e6I0wlpxpao" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'}
-  },
+  },*/
   ];
 
 function getData() {
