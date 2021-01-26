bf85bb6fd409004e002a5be4ee15aa1ca7aba66b
DrKendall332 <67517792+DrKendall332@users.noreply.github.com>
2021-01-26 09:25:12 +0000

    Update n1_07.js

--- Images/n1_07.js
+++ Images/n1_07.js
@@ -14,7 +14,7 @@ var markers = [{
     scale: [1,2],
 	data: {type: '3Droom', room: 'pe', id:'N1.07', x:0, y:0}
   },
-  {
+  /*{
     id: 'MusicVid',
     y: 1270,
     x: 465,
@@ -24,7 +24,7 @@ var markers = [{
     tooltip: 'Music Introduction',
     scale: [1,2],
     data: {type: 'gameFrame', html: '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/MVB2NnBdfp0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'}
-  },
+  },*/
   ];
 
 function getData() {
